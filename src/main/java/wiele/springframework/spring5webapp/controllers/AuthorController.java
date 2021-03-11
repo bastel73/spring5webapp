bce101770b5f546bda85a128da82c7a71df249b2
sebastian <sebastian@wiele-ol.de>
2021-03-11 22:25:59 +0100

    mvc setup

---
+++ src/main/java/wiele/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package wiele.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import wiele.springframework.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+    final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
